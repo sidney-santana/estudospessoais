aecae8d34b659f9c6b4b852aff91e2f4803aa737
sidney-santana <sidney.santana@aluno.ufabc.edu.br>
2022-11-07 22:04:37 -0300

    testando

--- teste.py
+++ teste.py
@@ -1,3 +1,12 @@
-print('oi')
-
+import random
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+n5 = str(input('Quinto aluno: '))
+lista = [n1,n2,n3,n4,n5]
+random.shuffle(lista)
+lista2 = random.choice(lista)
+print(lista)
+print('A pessoa mais querida da sala é você {}'.format(lista2))
 
