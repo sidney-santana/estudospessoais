3d70fa40cc2cd9fef8404be87d7fe97ac50016a1
sidney-santana <sidney.santana@aluno.ufabc.edu.br>
2022-11-17 21:11:17 -0300

    Aula 11 - Curso python

---
+++ cpcondicoes.py
@@ -0,0 +1,6 @@
+tempo = int(input('Quantos anos tem seu carro?'))
+if tempo <=3:#
+    print('carro novo')# print('carro novo' if tempo<==3 else'carro velho')
+else:#
+    print('carro velho')#
+print('--FIM--')
